c3c4e293575ebb117b8ca58c260e62a00b9e94a9
chiranjeevi siva deepak <deepuainmapudi405@gmail.com>
2025-12-19 14:16:37 +0530

    second

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "50mb" })); 
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://ai-frontend-seven-drab.vercel.app" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
@@ -39,7 +39,7 @@ app.get("/api/chat/group-history", async (req, res) => {
 });
 
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "https://ai-frontend-seven-drab.vercel.app" },
   maxHttpBufferSize: 1e8, // Allow up to 100MB buffer for file transfers
 });
 
@@ -88,4 +88,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+server.listen(PORT, () => console.log("✅ Server running on https://ai-frontend-seven-drab.vercel.app/"));
